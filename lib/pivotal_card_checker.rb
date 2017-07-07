8ae9e2694c3d38cd852e2b1c3a308c0d9f5132e5
Vlad Kaganyuk <vkaganyuk@hedgeye.com>
2017-07-07 12:39:08 -0700

    Cleaned up code with Rubocop, and added another test case.

--- lib/pivotal_card_checker.rb
+++ lib/pivotal_card_checker.rb
@@ -1,5 +1,5 @@
-require "pivotal_card_checker/version"
-require "tracker_api"
+require 'pivotal_card_checker/version'
+require 'tracker_api'
 
 module PivotalCardChecker
   MISSING_PROD_TYPE = 1
@@ -8,27 +8,28 @@ module PivotalCardChecker
   OTHER_ISSUE_TYPE = 4
 
   class CardChecker
-    HEDGEYE_PROJECT_ID = 414867
+    attr_accessor :api_key, :proj_id
 
-  #  def checkCards
+    def initialize(api_key = 0, proj_id = 0)
+      @api_key = api_key
+      @proj_id = proj_id
+      @all_stories = Hash.new {}
+      @all_labels = Hash.new {}
+      @all_comments = Hash.new {}
+      @all_owners = Hash.new {}
+    end
 
-    def self.checkCards(api_key)
-      #card_checker = new(proj_id, api_key)
-      #card_checker.checkCards
+    def check_cards
       client = TrackerApi::Client.new(token: api_key)
-      hedgeye_project = client.project(HEDGEYE_PROJECT_ID)
+      hedgeye_project = client.project(proj_id)
 
-      #Stores all bad card info. Maps owner string to BadCardManager.
-      bad_card_info = Hash.new
+      # Stores all bad card info. Maps owner string to BadCardManager.
+      bad_card_info = Hash.new {}
 
-      #Gets the current iteration and all backlog iterations.
+      # Gets the current iteration and all backlog iterations.
       iterations = hedgeye_project.iterations(scope: :current_backlog)
 
       should_have_to_prod = []
-      @all_stories = Hash.new
-      @all_labels = Hash.new
-      @all_comments = Hash.new
-      @all_owners = Hash.new
 
       iterations.each do |iteration|
         iteration.stories.each do |story|
@@ -40,145 +41,148 @@ module PivotalCardChecker
         end
       end
 
-      findCandidateStories(should_have_to_prod)
-      analyzeCandidates(should_have_to_prod, bad_card_info)
-      printReport(bad_card_info)
+      find_candidate_stories(should_have_to_prod)
+      analyze_candidates(should_have_to_prod, bad_card_info)
+      print_report(bad_card_info)
     end
 
-    def self.findCandidateStories(should_have_to_prod)
+    def self.check_cards(api_key, proj_id)
+      card_checker = new(api_key, proj_id)
+      card_checker.check_cards
+    end
+
+    def find_candidate_stories(should_have_to_prod)
       @all_stories.each do |story_id, story|
         state = story.current_state
-        if state === "finished" or state === "delivered" or state === "accepted"
+        if state == 'finished' || state == 'delivered' || state == 'accepted'
           should_have_to_prod.push(story)
-        else
-          if searchComments(story_id, "Commit by") != "not found"
-            should_have_to_prod.push(story)
-          end
+        elsif search_comments(story_id, 'Commit by') != 'not found'
+          should_have_to_prod.push(story)
         end
       end
     end
 
-    def self.analyzeCandidates(should_have_to_prod, bad_card_info)
+    def analyze_candidates(should_have_to_prod, bad_card_info)
       should_have_to_prod.each do |story|
         story_id = story.id
-        card_owners = getOwners(story_id)
-        if story.current_state === "accepted"
-          if (searchComments(story_id, "prod acceptance") === "not found" and
-            getSystemLabelFromCommit(story_id) != "sysLabelUnknown") and hasLabel(story_id, "to_prod")
+        card_owners = get_owners(story_id)
+        if story.current_state == 'accepted'
+          if (search_comments(story_id, 'prod acceptance') == 'not found' &&
+            get_system_label_from_commit(story_id) != 'sysLabelUnknown') &&
+            has_label?(story_id, 'to_prod')
             if bad_card_info[card_owners].nil?
               bad_card_info[card_owners] = BadCardManager.new
             end
-              bad_card_info[card_owners].addCard(BadCard.new(OTHER_ISSUE_TYPE, story.name,
-              "https://www.pivotaltracker.com/story/show/#{story_id}",
-              "Card is marked 'accepted', but doesn't have prod acceptance"))
+            bad_card_info[card_owners].add_card(BadCard.new(OTHER_ISSUE_TYPE,
+                                                            story.name,
+                                                            "https://www.pivotaltracker.com/story/show/#{story_id}",
+                                                            'Card is marked \'accepted\', but doesn\'t have prod acceptance'))
           end
           next
         end
         has_prod_label = false
         has_sys_label = false
         needs_criteria = true
-        sys_label_detected = "None"
-        sys_label_from_commit = getSystemLabelFromCommit(story_id)
+        sys_label_detected = 'None'
+        sys_label_from_commit = get_system_label_from_commit(story_id)
         temp = []
 
         @all_labels[story_id].each do |label|
-          if label.name === "to_prod"
+          if label.name == 'to_prod'
             has_prod_label = true
-          elsif label.name === "not_to_prod" or label.name === "delayed_prod"
+          elsif label.name == 'not_to_prod' || label.name == 'delayed_prod'
             has_prod_label = true
             needs_criteria = false
           end
-          if isSystemLabel(label.name)
+          if system_label?(label.name)
             sys_label_detected = label.name
             has_sys_label = true
           end
         end
 
-        #If the card doesn't have a to_prod label.
+        # If the card doesn't have a to_prod label.
         if !has_prod_label
           temp.push(BadCard.new(MISSING_PROD_TYPE, story.name,
-          "https://www.pivotaltracker.com/story/show/#{story_id}"))
-        elsif needs_criteria and (story.current_state === "finished" or story.current_state === "delivered")
-          message = "None"
-          if !hasAcceptanceCriteria(story_id)
-            message = "No acceptance criteria in desciption or comments."
-          elsif hasLabel(story_id, "criteria needed")
-            message = "'criteria needed' label was detected."
+                                "https://www.pivotaltracker.com/story/show/#{story_id}"))
+        elsif needs_criteria && (story.current_state == 'finished' || story.current_state == 'delivered')
+          message = 'None'
+          if !has_acceptance_criteria(story_id)
+            message = 'No acceptance criteria in desciption or comments.'
+          elsif has_label?(story_id, 'criteria needed')
+            message = '\'criteria needed\' label was detected.'
           end
 
-          if message != "None"
+          if message != 'None'
             temp.push(BadCard.new(MISSING_CRITERIA_TYPE, story.name,
-            "https://www.pivotaltracker.com/story/show/#{story_id}", message))
+                                  "https://www.pivotaltracker.com/story/show/#{story_id}", message))
           end
         end
 
         if !has_sys_label
-          message = "No system labels detected (reader, cms, dct, etc...)"
-          if sys_label_from_commit != "sysLabelUnknown"
+          message = 'No system labels detected (reader, cms, dct, etc...)'
+          if sys_label_from_commit != 'sysLabelUnknown'
             message = "Did not find expected label: '#{sys_label_from_commit}'"
           end
 
           temp.push(BadCard.new(MISSING_SYS_LABEL_TYPE, story.name,
-          "https://www.pivotaltracker.com/story/show/#{story_id}", message))
-        else
-          if sys_label_from_commit != "sysLabelUnknown" and !hasLabel(story_id, sys_label_from_commit)
-            temp.push(BadCard.new(MISSING_SYS_LABEL_TYPE, story.name,
-            "https://www.pivotaltracker.com/story/show/#{story_id}",
-            "Expected label: '#{sys_label_from_commit}', but found '#{sys_label_detected}' instead."))
-          end
+                                "https://www.pivotaltracker.com/story/show/#{story_id}", message))
+        elsif sys_label_from_commit != 'sysLabelUnknown' && !has_label?(story_id, sys_label_from_commit)
+          temp.push(BadCard.new(MISSING_SYS_LABEL_TYPE, story.name,
+                                "https://www.pivotaltracker.com/story/show/#{story_id}",
+                                "Expected label: '#{sys_label_from_commit}', but found '#{sys_label_detected}' instead."))
         end
 
-        if story.current_state === "finished" and sys_label_from_commit === "sysLabelUnknown"
+        if story.current_state == 'finished' && sys_label_from_commit == 'sysLabelUnknown'
           temp.push(BadCard.new(OTHER_ISSUE_TYPE, story.name,
-          "https://www.pivotaltracker.com/story/show/#{story_id}",
-          "Card is marked 'finished', but has no commits."))
-        elsif story.current_state === "delivered" and searchComments(story_id, "staging acceptance") === "not found"
+                                "https://www.pivotaltracker.com/story/show/#{story_id}",
+                                'Card is marked \'finished\', but has no commits.'))
+        elsif story.current_state == 'delivered' && search_comments(story_id, 'staging acceptance') == 'not found'
           temp.push(BadCard.new(OTHER_ISSUE_TYPE, story.name,
-          "https://www.pivotaltracker.com/story/show/#{story_id}",
-          "Card is marked 'delivered', but doesn't have staging acceptance"))
+                                "https://www.pivotaltracker.com/story/show/#{story_id}",
+                                'Card is marked \'delivered\', but doesn\'t have staging acceptance'))
         end
 
-        if temp.length > 0
+        unless temp.empty?
           if bad_card_info[card_owners].nil?
             bad_card_info[card_owners] = BadCardManager.new
           end
           temp.each do |card|
-            bad_card_info[card_owners].addCard(card)
+            bad_card_info[card_owners].add_card(card)
           end
         end
       end
     end
 
-    def self.printReport(bad_card_info)
+    def print_report(bad_card_info)
       puts "\n========= Results ==========="
-      if bad_card_info.length == 0
-        puts "CONGRATS! No card violations."
+      if bad_card_info.empty?
+        puts 'CONGRATS! No card violations.'
       else
         bad_card_info.each do |owner_name, card_manager|
           puts "OWNER(s): #{owner_name}"
-          if card_manager.missing_prod_label.length > 0
-            puts "        Missing prod description label ('to_prod', 'delayed_prod', or 'not_to_prod'):"
+          unless card_manager.missing_prod_label.empty?
+            puts '        Missing prod description label (\'to_prod\', \'delayed_prod\', or \'not_to_prod\'):'
             card_manager.missing_prod_label.each do |card|
               puts "                #{card.title} - #{card.link}"
             end
           end
 
-          if card_manager.missing_sys_label.length > 0
-            puts "        Missing system label:"
+          unless card_manager.missing_sys_label.empty?
+            puts '        Missing system label:'
             card_manager.missing_sys_label.each do |card|
               puts "                #{card.title} - #{card.link} - #{card.message}"
             end
           end
 
-          if card_manager.missing_criteria.length > 0
-            puts "        Missing acceptance criteria:"
+          unless card_manager.missing_criteria.empty?
+            puts '        Missing acceptance criteria:'
             card_manager.missing_criteria.each do |card|
               puts "                #{card.title} - #{card.link} - #{card.message}"
             end
           end
 
-          if card_manager.other_issues.length > 0
-            puts "        Other issues:"
+          unless card_manager.other_issues.empty?
+            puts '        Other issues:'
             card_manager.other_issues.each do |card|
               puts "                #{card.title} - #{card.link} - #{card.message}"
             end
@@ -188,60 +192,58 @@ module PivotalCardChecker
       end
     end
 
-    #Returns a comma seperated list of all the story's owners.
-    def self.getOwners(story_id)
+    # Returns a comma seperated list of all the story's owners.
+    def get_owners(story_id)
       owner_names = []
       @all_owners[story_id].each do |person|
         owner_names.push(person.name)
       end
 
-      owner_names.join(", ")
+      owner_names.join(', ')
     end
 
-    #Returns true if the given label name is a system label.
-    def self.isSystemLabel(labelName)
-      labelName === "cms" or labelName === "billing engine" or labelName === "dct" or
-      labelName === "reader" or labelName === "marketing" or labelName === "ui" or
-      labelName === "pivotal card health tools" or labelName === "mailroom"
+    # Returns true if the given label name is a system label.
+    def system_label?(label_name)
+      label_name == 'cms' || label_name == 'billing engine' || label_name == 'dct' ||
+      label_name == 'reader' || label_name == 'marketing' || label_name == 'ui' ||
+      label_name == 'pivotal card health tools' || label_name == 'mailroom'
     end
 
-    def self.getSystemLabelFromCommit(story_id)
-      temp = "sysLabelUnknown"
-      searchResult = searchComments(story_id, "github.com/")
+    def get_system_label_from_commit(story_id)
+      temp = 'sysLabelUnknown'
+      search_result = search_comments(story_id, 'github.com/')
 
-      if searchResult != "not found"
-        temp = searchResult.split(/github.com\/(.*?)\/(.*?)\/commit/)[2]
-        if temp.include? "hedgeye-"
+      if search_result != 'not found'
+        temp = search_result.split(/github.com\/(.*?)\/(.*?)\/commit/)[2]
+        if temp.include? 'hedgeye-'
           temp = temp[8...temp.length]
         end
-        temp.gsub!('_', ' ')
+        temp.tr!('_', ' ')
       end
       result = temp
     end
 
-    def self.hasAcceptanceCriteria(story_id)
+    def has_acceptance_criteria(story_id)
       story = @all_stories[story_id]
-      hasCriteria = false
+      has_criteria = false
 
-      #Check in labels for 'criteria approved'
-      if hasLabel(story_id, "criteria approved")
-        hasCriteria = true;
-      #Check for acceptance criteria in card descrption.
-      elsif !story.description.nil? and story.description.downcase.include? "acceptance criteria"
-        hasCriteria = true
-      #Criteria not found in description, check comments.
-      else
-        if searchComments(story_id, "acceptance criteria") != "not found"
-          hasCriteria = true
-        end
+      # Check in labels for 'criteria approved'
+      if has_label?(story_id, 'criteria approved')
+        has_criteria = true
+      # Check for acceptance criteria in card descrption.
+      elsif !story.description.nil? && (story.description.downcase.include? 'acceptance criteria')
+        has_criteria = true
+      # Criteria not found in description, check comments.
+      elsif search_comments(story_id, 'acceptance criteria') != 'not found'
+        has_criteria = true
       end
-      temp = hasCriteria
+      temp = has_criteria
     end
 
-    def self.hasLabel(story_id, label_looking_for)
+    def has_label?(story_id, label_looking_for)
       found = false
       @all_labels[story_id].each do |label|
-        if label.name === label_looking_for
+        if label.name == label_looking_for
           found = true
           break
         end
@@ -249,10 +251,10 @@ module PivotalCardChecker
       result = found
     end
 
-    def self.searchComments(story_id, searchString)
-      temp = "not found"
+    def search_comments(story_id, search_string)
+      temp = 'not found'
       @all_comments[story_id].each do |comment|
-        if !comment.text.nil? and comment.text.downcase.include? searchString
+        if !comment.text.nil? && (comment.text.downcase.include? search_string)
           temp = comment.text
           break
         end
@@ -271,7 +273,7 @@ module PivotalCardChecker
       @other_issues = []
     end
 
-    def addCard(card)
+    def add_card(card)
       case card.type
       when MISSING_PROD_TYPE
         @missing_prod_label.push(card)
@@ -282,7 +284,7 @@ module PivotalCardChecker
       when OTHER_ISSUE_TYPE
         @other_issues.push(card)
       else
-        puts "Card not valid type."
+        puts 'Card not valid type.'
       end
     end
   end
@@ -290,7 +292,7 @@ module PivotalCardChecker
   class BadCard
     attr_accessor :type, :title, :link, :message
 
-    def initialize(type = 0, title = "Default title", link = "Default link", message = nil)
+    def initialize(type = 0, title = 'Default title', link = 'Default link', message = nil)
       @type = type
       @title = title
       @link = link
--- spec/pre_refactoring_test_spec.rb
+++ spec/pre_refactoring_test_spec.rb
@@ -13,13 +13,25 @@ end
 
 class CardChecker
   describe 'Check the cards for any errors' do
-    it 'should say that there are two missing prod labels for Alex.' do
-      VCR.use_cassette 'pre_refactoring_reponse' do
+    it 'should produce output that matches the text in pre_refactoring_test_output.txt' do
+      # Cassette recorded July 6, 2017
+      VCR.use_cassette 'multiple_card_violations_reponse' do
         printed = capture_stdout do
-          PivotalCardChecker::CardChecker.checkCards('insert api key')
+          PivotalCardChecker::CardChecker.check_cards('using cassette, no need for api key', 414_867)
         end
 
-       printed.should eq(IO.read('spec/pre_refactoring_test_output.txt'))
+        printed.should eq(IO.read('spec/expected_output/multiple_card_violations_output.txt'))
+      end
+    end
+
+    it 'should produce output that matches the text in no_card_violations_output.txt' do
+      # Cassette recorded July 7, 2017
+      VCR.use_cassette 'no_card_violations' do
+        printed = capture_stdout do
+          PivotalCardChecker::CardChecker.check_cards('using cassette, no need for api key', 414_867)
+        end
+
+        printed.should eq(IO.read('spec/expected_output/no_card_violations_output.txt'))
       end
     end
   end
