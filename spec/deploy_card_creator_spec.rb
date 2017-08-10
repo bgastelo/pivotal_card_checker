47973b973c98e2c4f3522a47943ab21dec095dfb
Vlad Kaganyuk <vkaganyuk@hedgeye.com>
2017-08-10 12:03:56 -0400

    Added another test case to test dct and mailroom labels

--- spec/deploy_card_creator_spec.rb
+++ spec/deploy_card_creator_spec.rb
@@ -14,8 +14,8 @@ describe PivotalCardChecker::DeployCardCreator do
     expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} cms deploy")
     expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_8_9_2017.txt'))
 
-    correct_system_labels = ['cms']
-    expect(correct_system_labels - @card_labels).to eql([])
+    correct_system_labels = ['cms', 'deploy']
+    expect(@card_labels.sort).to eql(correct_system_labels.sort)
   end
 
   it 'should create a deploy card with consolidated labels under the website redo epic' do
@@ -27,8 +27,8 @@ describe PivotalCardChecker::DeployCardCreator do
     expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} cms, reader deploy")
     expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_creation_8_9_2017_consolidation_test_epic.txt'))
 
-    correct_system_labels = ['cms']
-    expect(correct_system_labels - @card_labels).to eql([])
+    correct_system_labels = ['cms', 'deploy', 'reader']
+    expect(@card_labels.sort).to eql(correct_system_labels.sort)
   end
 
   it 'should create a deploy card with consolidated labels not under any epic' do
@@ -40,7 +40,20 @@ describe PivotalCardChecker::DeployCardCreator do
     expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} cms, reader deploy")
     expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_creation_8_9_2017_consolidation_test_reg.txt'))
 
-    correct_system_labels = ['cms']
-    expect(correct_system_labels - @card_labels).to eql([])
+    correct_system_labels = ['cms', 'deploy', 'reader']
+    expect(@card_labels.sort).to eql(correct_system_labels.sort)
+  end
+
+  it 'should create a deploy card with cms and reader urls, but regular dct and mailroom labels.' do
+    VCR.use_cassette 'deploy_card_creation_8_10_2017_label_urls' do
+      @card_title, @card_description, @card_labels =
+        PivotalCardChecker::CardChecker.create_deploy_card('using cassette', 414_867, [DEPLOY_LABEL_ID])
+    end
+
+    expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} cms, dct, reader, mailroom deploy")
+    expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_creation_8_10_2017_label_urls.txt'))
+
+    correct_system_labels = ['cms', 'dct', 'deploy', 'reader', 'mailroom']
+    expect(@card_labels.sort).to eql(correct_system_labels.sort)
   end
 end
