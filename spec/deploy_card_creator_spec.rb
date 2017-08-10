1a57ed88ccd7c5149be12a29c07d7a46b95a5d2a
Vlad Kaganyuk <vkaganyuk@hedgeye.com>
2017-08-10 09:38:29 -0400

    Deploy card creator now consolidates card labels.

--- spec/deploy_card_creator_spec.rb
+++ spec/deploy_card_creator_spec.rb
@@ -5,29 +5,42 @@ require 'spec_helper'
 describe PivotalCardChecker::DeployCardCreator do
   DEPLOY_LABEL_ID = 2_506_935
 
-  it 'should create one deploy card with the properties tested below' do
-    VCR.use_cassette 'deploy_card_creation_7_19_2017' do
+  it 'should create one deploy card with just one cms story' do
+    VCR.use_cassette 'deploy_card_creation_8_9_2017' do
       @card_title, @card_description, @card_labels =
         PivotalCardChecker::CardChecker.create_deploy_card('using cassette', 414_867, [DEPLOY_LABEL_ID])
     end
 
-    expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} reader, cms deploy")
-    expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_7_19_2017.txt'))
+    expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} cms deploy")
+    expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_8_9_2017.txt'))
 
-    correct_system_labels = ['cms', 'deploy', 'reader']
+    correct_system_labels = ['cms']
     expect(correct_system_labels - @card_labels).to eql([])
   end
 
-  it 'should create a deploy card description with multiple cards under cms, which is under website redo.' do
-    VCR.use_cassette 'deploy_card_creation_7_25_2017' do
-      @card_title, @card_description, @card_labels = 
+  it 'should create one deploy card with fsfsd' do
+    VCR.use_cassette 'deploy_card_creation_8_9_2017_consolidation_test_epic' do
+      @card_title, @card_description, @card_labels =
         PivotalCardChecker::CardChecker.create_deploy_card('using cassette', 414_867, [DEPLOY_LABEL_ID])
     end
 
-    expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} cms, billing engine deploy")
-    expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_7_25_2017.txt'))
+    expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} cms, reader deploy")
+    expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_creation_8_9_2017_consolidation_test_epic.txt'))
 
-    correct_system_labels = ['billing engine', 'cms', 'deploy']
+    correct_system_labels = ['cms']
+    expect(correct_system_labels - @card_labels).to eql([])
+  end
+
+  it 'should create one deploy card with lkj' do
+    VCR.use_cassette 'deploy_card_creation_8_9_2017_consolidation_test_reg' do
+      @card_title, @card_description, @card_labels =
+        PivotalCardChecker::CardChecker.create_deploy_card('using cassette', 414_867, [DEPLOY_LABEL_ID])
+    end
+
+    expect(@card_title).to eql("#{Time.now.strftime('%-m/%-d/%y')} cms, reader deploy")
+    expect(@card_description).to eql(IO.read('spec/expected_output/deploy_card_creation_8_9_2017_consolidation_test_reg.txt'))
+
+    correct_system_labels = ['cms']
     expect(correct_system_labels - @card_labels).to eql([])
   end
 end
