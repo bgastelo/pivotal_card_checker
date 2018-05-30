1248d90d6c36dc7a04cfa6650ecb71933dce0961
Forrest Chang <fchang@hedgeye.com>
2018-05-29 19:38:07 -0700

    [#157883379] don't check iteration coz we usually fall out of it

--- lib/pivotal_card_checker/checkers/all_cards_assigned_checker.rb
+++ lib/pivotal_card_checker/checkers/all_cards_assigned_checker.rb
@@ -8,7 +8,7 @@ module PivotalCardChecker
       def check
         @results = []
         @all_story_cards.each do |story_card|
-          @results << story_card if story_card.owners.empty? && story_card.in_current_iteration
+          @results << story_card if story_card.owners.empty? # && story_card.in_current_iteration
         end
         @results
       end
